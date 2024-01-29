59f72541b04ba8dedd0872b8013fa75361f1105f
riviereteo <120677744+riviereteo@users.noreply.github.com>
2024-01-29 21:59:51 +0100

    lancements test connexion base de donnée

--- index.php
+++ index.php
@@ -10,19 +10,18 @@
 </head>
 
 <body>
-<!-- Gameteoriviere33 -->
-<!-- php -S 127.0.0.1:8080 -->
-<?php
-$dsn = "mysql:dbname=game;host=teoriviere.com";
-$user = 'game';
-$password = 'Gameteoriviere33';
-$pdo = new PDO($dsn, $user, $password);
-$sql = "SELECT * FROM test";
-$query = $pdo->query($sql);
-foreach ($query as $row) {
+    <!-- php -S 127.0.0.1:8080 -->
+    <?php
+    $dsn = "mysql:dbname=u611830779_game;host=89.116.147.52";
+    $user = 'u611830779_game';
+    $password = 'Gameteoriviere33';
+    $pdo = new PDO($dsn, $user, $password);
+    $sql = "SELECT `Test`.`testString`FROM `Test`;";
+    $query = $pdo->query($sql);
+    foreach ($query as $row) {
     echo 'Nom : ' . $row['testString']. "\n";
-}
-?>
+    }
+    ?>
     <script src="index.js"></script>
 </body>
 
