f12868b045b83ac56bf8b1bf9880636b8689308c
riviereteo <120677744+riviereteo@users.noreply.github.com>
2024-02-18 15:48:03 +0100

    add zoom into expériences

---
+++ zoom/index.js
@@ -0,0 +1,8 @@
+const valueBar = document.getElementById('valueBar');
+valueBar.addEventListener('input', (e) => {
+    const zoomLevel = e.target.value;
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+    document.body.style.backgroundSize = `${zoomLevel}px ${zoomLevel}px`;
+    document.body.style.backgroundPosition = `${centerX}px ${centerY}px`;
+});
