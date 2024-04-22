08df01cb6f25e682c6bd82430a9d03eaf2c914f1
riviereteo <120677744+riviereteo@users.noreply.github.com>
2024-04-22 23:26:42 +0200

    push new arbo idea and change local img of aie...

---
+++ shapes/index.js
@@ -0,0 +1,53 @@
+const size = 10;
+const drawnPositions = [];
+
+function dessinerTriangle(ctx, x, y, rotation) {
+
+    const height = (size * Math.sqrt(3)) / 2;
+    const x2 = x + size;
+    const y2 = y;
+    const x3 = x + size / 2;
+    let y3 = y;
+    rotation ? y3 += height : y3 -= height;
+
+    const positionString = `${x},${y},${rotation}`;
+    drawnPositions.push(positionString);
+
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x2, y2);
+    ctx.lineTo(x3, y3);
+    ctx.closePath();
+    ctx.stroke();
+    
+    trouverPositionSuivant(ctx, x, y, rotation, height);
+}
+
+window.onload = () => {
+    const canvas = document.getElementById('myCanvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+
+    const x = window.innerWidth / 2;
+    const y = window.innerHeight / 2;
+    const rotation = true;
+    dessinerTriangle(ctx, x, y, rotation);
+}
+
+function trouverPositionSuivant(ctx, x, y, rotation, height) {
+    const randomFace = Math.floor(Math.random() * 3);
+    switch (randomFace) {
+        case 0:
+            dessinerTriangle(ctx, x, y, !rotation);
+            break;
+        case 1:
+            rotation ? dessinerTriangle(ctx, x - size / 2, y + height, !rotation) : dessinerTriangle(ctx, x - size / 2, y - height, !rotation);
+            break;
+        case 2:
+            rotation ? dessinerTriangle(ctx, x + size / 2, y + height, !rotation) : dessinerTriangle(ctx, x + size / 2, y - height, !rotation);
+            break;
+        default:
+            break;
+    }
+}
